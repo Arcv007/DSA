f4e7f54048e65f51bb6cf45bb8e855422933fb01
Rohit <csai21090@glbitm.ac.in>
2023-02-07 02:40:11 +0530

    regular

---
+++ C++/Pattern/prajwal.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"enter the number"<<endl;
+    cin>>n;
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<+i;j++)
+        {
+            if((i+j)%2==0)
+                cout<<"0";
+            else
+                cout<<"1";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
