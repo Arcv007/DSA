631b3f79c63ff60b132689b26ee45671bee668ca
Rohit <csai21090@glbitm.ac.in>
2023-02-19 00:27:56 +0530

    least complexity

--- C++/Math/Prime.cpp
+++ C++/Math/Prime.cpp
@@ -2,14 +2,33 @@
 #include<bits/stdc++.h>
 using namespace std; 
 
-// To check Prime or not
-bool prime(int n)
+// To check Prime or not (Naive Solution)
+/* bool prime(int n)
 {
     if(n==1)
         return false;
     for (int i=2; i<sqrt(n); i++)
     {
         if(n%i==0)
+            return false;
+    }
+    return true;
+
+} */
+
+// Optimized Solution
+bool prime(int n)
+{
+    if(n==1)
+        return false;
+    else if (n==2 || n==3)
+        return true;
+    else if (n%2==0 || n%3==0)
+        return false;
+            
+    for (int i=5; i*i<n; i+6)
+    {
+        if(n%i==0 || n%(i+2)==0)
             return false;
     }
     return true;
