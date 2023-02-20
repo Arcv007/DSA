a6f0f615ee10b4c7a787f92acb832b2ee0cb17de
Rohit <csai21090@glbitm.ac.in>
2023-02-21 01:49:55 +0530

    C++: All Divisor

---
+++ C++/Math/All_Divisor.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std; 
+
+int main()
+{
+    int num;
+    cout<<"Enter the number: ";
+    cin>>num;
+    for (int i=1; i<=num ; i++)
+    {
+        if(num%i==0)
+            cout<<endl<<i<<" ";
+    }
+    return 0;
+}
