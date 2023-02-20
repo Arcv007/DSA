4c0ff5ad435b919476b007c7aab61f906318229c
Rohit <csai21090@glbitm.ac.in>
2023-02-21 01:50:49 +0530

    C++: Prime Naive solution

---
+++ C++/Math/Prime_01.cpp
@@ -0,0 +1,30 @@
+// write a program to check weather the number is prime number or not
+#include<bits/stdc++.h>
+using namespace std; 
+
+// To check Prime or not (Naive Solution)
+bool prime(int n)
+{
+    if(n==1)
+        return false;
+    for (int i=2; i<sqrt(n); i++)
+    {
+        if(n%i==0)
+            return false;
+    }
+    return true;
+
+}
+
+
+int main()
+{
+    int num;
+    cout<<"Enter the number: "<<endl;
+    cin>>num;
+    if(prime(num))
+        cout<<"Prime number"<<endl;
+    else
+        cout<<"Composite number";
+    return 0;
+}
