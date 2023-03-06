b29679996cd65a78447c827815812acf52710b16
Rohit <csai21090@glbitm.ac.in>
2023-03-06 23:59:23 +0530

    Change in comment

--- C++/Bit Manupulation/NOT.cpp
+++ C++/Bit Manupulation/NOT.cpp
@@ -10,14 +10,3 @@ int main()
     cout<<"\nThe NOT operation of a numbers are : "<<(~x);
     return 0;
 }
-
-/* 
-eg.  3 in binary = 00..00011
-     6 in binary = 00..00110
-
-     so,     3 & 6  == 2
-
-                    0011
-                    0110
-                    ----
-                    0101  ==  5 in decimal */
